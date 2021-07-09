16afb3c4dfe825aa67447015a7db73bbd44408fc
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-10 00:30:08 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Eje21
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese el dia de la semana sin acentos: ");
+            string dia = Console.ReadLine().ToLower();
+            switch (dia)
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("Día laboral.");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Día no laboral");
+                    break;
+                default:
+                    Console.WriteLine("Día de la semana no existe");
+                    break;
+
+            }
         }
     }
 }
